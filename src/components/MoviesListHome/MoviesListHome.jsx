98a664ace0e29a0c1b2d86e1139d5635e83c4410
DangirasH <97556742+DangirasH@users.noreply.github.com>
2022-10-05 16:33:42 +0200

    Structure and style of cards complete + tailwind settings + images added (#6)
    * Testing for Sylvain
    
    * added images and cards to the home page + structure complete
    
    * fixed extra code + nesting

---
+++ src/components/MoviesListHome/MoviesListHome.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+import "./MoviesListHome.scss";
+
+function MoviesListHome() {
+  return (
+    // Card for movies
+    <div>
+      <div className="movieList mt-5">
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="movieList mt-5">
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-lg shadow-lg bg-white max-w-sm">
+            <a href="#!">
+              <img
+                className="rounded-t-lg"
+                src="/images/lucifer.jpeg"
+                alt="lucifer"
+              />
+            </a>
+            <div className="movieTitle">
+              <h2 className="text-gray-1000 text-xl font-medium">Lucifer</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MoviesListHome;
