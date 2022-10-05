311df9ca37ca748c979b032e9b3cbf87db2b02ee
kylian moreira <kylianmoreira@gmail.com>
2022-10-05 12:07:05 +0200

    structure navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import "tw-elements";
 import Home from "./components/home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar />
       <Home />
     </>
   );
