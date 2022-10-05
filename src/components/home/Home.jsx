136b0421018f8b44b805d78401d85e5e7f768a58
DangirasH <97556742+DangirasH@users.noreply.github.com>
2022-10-05 10:09:17 +0200

    Home Page color + video header + sass installed (#1)
    * home page colour + sass + header video
    
    * replaced npm code by yarn codes
    
    * fixed readme
    
    * fixed
    
    * fixed

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div>
+      <body className="home">
+        <video src="/videos/video2.mp4" autoPlay loop muted />
+        <h1>navbar</h1>
+      </body>
+    </div>
+  );
+}
+export default Home;
