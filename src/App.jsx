77ad73384711b7d581ec7d0c417783d4175fb2a0
SkeidoK <98970084+SkeidoK@users.noreply.github.com>
2022-10-05 17:30:13 +0200

    S01 navbar (#4)
    * structure navbar
    
    * yarn format
    
    * chenge request

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
       <Footer />
     </>
