4f9708d8cb2c5c1d771c6d2b3d1a75922d9478bd
SkeidoK <98970084+SkeidoK@users.noreply.github.com>
2022-10-05 16:39:56 +0200

    footer structure and style (#5)
    * footer structure and style
    
    * fomat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Home from "./components/home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
