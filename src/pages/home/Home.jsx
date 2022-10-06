4f15a9a3a7ae8230c11e9a95f350cc114c63f5a6
kylian moreira <kylianmoreira@gmail.com>
2022-10-06 15:15:37 +0200

    navbar header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import MoviesListHome from "../../components/MoviesListHome/MoviesListHome";
 function Home() {
   return (
     <div className="home">
+      <h1>navbar</h1>
       <video src="/videos/video2.mp4" autoPlay loop muted />
       <div className="card">
         <MoviesListHome />
