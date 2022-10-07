d01a471961020040fac11e4ff74d155d0486570d
Paul <108493821+paul-chatelain-fernandes@users.noreply.github.com>
2022-10-07 10:21:10 +0200

    Added all searchbar features (#8)
    * Added all searchbar features
    
    * Tailwind config edited
    
    * Added tailwind
    
    * cat of input searchbar deleted
    
    * cat input deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import "./App.css";
 import "tw-elements";
 import Home from "./pages/home/Home";
-
-import Footer from "./components/Footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
