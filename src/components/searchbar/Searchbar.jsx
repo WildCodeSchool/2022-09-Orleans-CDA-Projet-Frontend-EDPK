884033cfd7248132e09f146fe63aa701531415ff
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2022-10-10 15:41:53 +0200

    added genre filter

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,12 +1,13 @@
 import "./Searchbar.scss";
 import { useEffect, useState } from "react";
 
-const Searchbar = ({ media, genre }) => {
+const Searchbar = ({ media, genreId }) => {
   const [search, setSearch] = useState("");
   const [display, setDisplay] = useState("");
 
-  const movieGenres = {
+  const genres = {
     28: "Action",
+    10759: "Action & Adventure",
     12: "Adventure",
     16: "Animation",
     35: "Comedy",
@@ -17,47 +18,40 @@ const Searchbar = ({ media, genre }) => {
     14: "Fantasy",
     36: "History",
     27: "Horror",
-    10402: "Music",
-    9648: "Mystery",
-    10749: "Romance",
-    878: "Science Fiction",
-    10770: "TV Movie",
-    53: "Thriller",
-    10752: "War",
-    37: "Western",
-  };
-
-  const tvGenres = {
-    10759: "Action & Adventure",
-    16: "Animation",
-    35: "Comedy",
-    80: "Crime",
-    99: "Documentary",
-    18: "Drama",
-    10751: "Family",
     10762: "Kids",
+    10402: "Music",
     9648: "Mystery",
     10763: "News",
     10764: "Reality",
+    10749: "Romance",
+    878: "Science Fiction",
     10765: "Sci-Fi & Fantasy",
     10766: "Soap",
     10767: "Talk",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
     10768: "War & Politics",
     37: "Western",
   };
 
   const apiValues = {};
-  if (genre) {
+  if (genreId) {
+    for (const key in genres) {
+      if (key.includes(genreId)) {
+        apiValues.genre = genres[key].toLowerCase();
+      }
+    }
     switch (media) {
       case "movie":
-        apiValues.url = `https://api.themoviedb.org/3/discover/movie?api_key=c27f8ca565d1e0e0b28f3721fb54e05e&with_genres=28`;
+        apiValues.url = `https://api.themoviedb.org/3/discover/movie?api_key=c27f8ca565d1e0e0b28f3721fb54e05e&with_genres=${genreId}`;
         apiValues.media = "MOVIE";
-        apiValues.placeholder = "Search a movie...";
+        apiValues.placeholder = `Search a movie of ${apiValues.genre}...`;
         break;
       case "tv":
-        apiValues.url = `https://api.themoviedb.org/3/discover/tv?api_key=c27f8ca565d1e0e0b28f3721fb54e05e&with_genres=28`;
+        apiValues.url = `https://api.themoviedb.org/3/discover/tv?api_key=c27f8ca565d1e0e0b28f3721fb54e05e&with_genres=${genreId}`;
         apiValues.media = "TV SHOW";
-        apiValues.placeholder = "Search a TV show...";
+        apiValues.placeholder = `Search a TV show of ${apiValues.genre}...`;
         break;
       default:
         break;
@@ -76,6 +70,7 @@ const Searchbar = ({ media, genre }) => {
         break;
       default:
         apiValues.url = `https://api.themoviedb.org/3/search/multi?api_key=c27f8ca565d1e0e0b28f3721fb54e05e&language=en&page=1&include_adult=false&query=${search}`;
+        apiValues.placeholder = "Search a movie, a TV show, or a person...";
         break;
     }
   }
@@ -85,7 +80,6 @@ const Searchbar = ({ media, genre }) => {
       fetch(apiValues.url)
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           setDisplay(data.results);
         });
     }, 500);
@@ -97,7 +91,12 @@ const Searchbar = ({ media, genre }) => {
   return (
     <>
       <div className="search-input">
-        <input placeholder={apiValues.placeholder} onChange={handleSearch} />
+        <label htmlFor="search_home">Are you searching something ?</label>
+        <input
+          id="search_home"
+          placeholder={apiValues.placeholder}
+          onChange={handleSearch}
+        />
       </div>
       {display && (
         <div className="search-list">
