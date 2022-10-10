884b607fa8968c3814d67770f40a535d9f103258
Emmanuel PEREIRA <epereira777@gmail.com>
2022-10-10 11:38:43 +0200

    removed import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "tw-elements";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-import Movie from "./pages/detail/movie";
 
 function App() {
   return (
