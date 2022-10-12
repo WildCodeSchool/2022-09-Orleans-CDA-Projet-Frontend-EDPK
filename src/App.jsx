a5ce27a84ce7a3221046342c566fbbc24bd1f8bc
kylian moreira <kylianmoreira@gmail.com>
2022-10-12 10:30:55 +0200

    yarn format

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import "tw-elements";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Home />
+        <Footer />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
