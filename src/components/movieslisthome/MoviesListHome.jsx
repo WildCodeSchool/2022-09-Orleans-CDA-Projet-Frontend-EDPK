b507a4a848a75b0f6e517351dd740ad1cfc308a9
Dangiras <dan.hausmans@gmail.com>
2022-10-14 10:28:03 +0200

    added key

--- src/components/movieslisthome/MoviesListHome.jsx
+++ src/components/movieslisthome/MoviesListHome.jsx
@@ -6,6 +6,8 @@ const MoviesListHome = () => {
   const apiKey = import.meta.env.VITE_API_KEY;
   const url_trending = "https://api.themoviedb.org/3/trending/all/day?api_key=";
   const [trending, setTrending] = useState([]);
+  const [tv, setTv] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   async function getTrending(signal) {
     const response = await axios
@@ -23,6 +25,8 @@ const MoviesListHome = () => {
   }, []);
 
   useEffect(() => {
+    setMovies(trending?.filter((t) => t.media_type === "movie").slice(0, 10));
+    setTv(trending?.filter((t) => t.media_type === "tv").slice(0, 10));
     console.log(trending);
   }, [trending]);
 
@@ -31,49 +35,43 @@ const MoviesListHome = () => {
       <div className="movieList mt-5">
         <h2 className="mb-3">Trending Movies</h2>
         <div className="board">
-          {trending
-            ?.filter((t) => t.media_type === "movie")
-            .slice(0, 10)
-            .map((t) => (
-              <div class="flex p-2">
-                <div class="rounded-lg  max-w-sm">
-                  <a href="#!">
-                    <img
-                      class="rounded-t-lg"
-                      src={`https://image.tmdb.org/t/p/w500${t.poster_path}`}
-                      alt={t.original_title}
-                    />
-                  </a>
-                  <div class="pt-2">
-                    <h3 class="text-xl font-medium mb-2">{t.title}</h3>
-                  </div>
+          {movies.map((t) => (
+            <div key={t.id} className="flex p-2">
+              <div className="rounded-lg  max-w-sm">
+                <a href="#!">
+                  <img
+                    className="rounded-t-lg"
+                    src={`https://image.tmdb.org/t/p/w500${t.poster_path}`}
+                    alt={t.original_title}
+                  />
+                </a>
+                <div className="pt-2">
+                  <h3 className="text-xl font-medium mb-2">{t.title}</h3>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
       <div className="movieList mt-5">
         <h2 className="mb-3">Trending TV Shows</h2>
         <div className="board">
-          {trending
-            ?.filter((t) => t.media_type === "tv")
-            .slice(0, 10)
-            .map((t) => (
-              <div class="flex p-2">
-                <div class="rounded-lg  max-w-sm">
-                  <a href="#!">
-                    <img
-                      class="rounded-t-lg"
-                      src={`https://image.tmdb.org/t/p/w500${t.poster_path}`}
-                      alt={t.original_title}
-                    />
-                  </a>
-                  <div class="pt-2">
-                    <h3 class="text-xl font-medium mb-2">{t.name}</h3>
-                  </div>
+          {tv.map((t) => (
+            <div key={t.id} className="flex p-2">
+              <div className="rounded-lg  max-w-sm">
+                <a href="#!">
+                  <img
+                    className="rounded-t-lg"
+                    src={`https://image.tmdb.org/t/p/w500${t.poster_path}`}
+                    alt={t.original_title}
+                  />
+                </a>
+                <div className="pt-2">
+                  <h3 className="text-xl font-medium mb-2">{t.name}</h3>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
