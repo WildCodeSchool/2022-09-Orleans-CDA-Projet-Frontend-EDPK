e034ef640d5e96a55992f3d63febcd8cb01697fe
SkeidoK <98970084+SkeidoK@users.noreply.github.com>
2022-10-14 09:27:23 +0200

    dropdown movies (#18)
    * add category in dropdown
    
    * update navbar
    
    * add style
    
    * suppr react import

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import axios from "axios";
 
 const Navbar = () => {
+  const [isShow, setShow] = useState(false);
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${
+          import.meta.env.VITE_API_KEY
+        }&language=en-US`
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        setCategory(data.genres);
+      });
+  }, []);
+
   return (
     <div>
       <nav className="">
@@ -21,13 +38,13 @@ const Navbar = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   />
                 </svg>
@@ -36,13 +53,13 @@ const Navbar = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   aria-hidden="true"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
@@ -62,23 +79,70 @@ const Navbar = () => {
                     <li>
                       <Link
                         to="/"
-                        className="titles hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                        className="titles inline-flex hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                       >
                         Home
                       </Link>
                     </li>
-                    <li>
-                      <Link
-                        to="/"
-                        className="titles hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    <div className="relative inline-block text-left">
+                      <div>
+                        <li
+                          onClick={() => setShow(!isShow)}
+                          type="button"
+                          className="titles inline-flex w-full rounded-md px-4 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+                          id="menu-button"
+                          aria-expanded="true"
+                          aria-haspopup="true"
+                        >
+                          Movies
+                          <svg
+                            className="-mr-1 ml-2 h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </li>
+                      </div>
+
+                      <div
+                        className={`${
+                          isShow
+                            ? "transition ease-out duration-100 transform opacity-100 scale-100"
+                            : "transition ease-in duration-75 transform opacity-0 scale-95"
+                        } absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="menu-button"
+                        tabIndex="-1"
                       >
-                        Movies
-                      </Link>
-                    </li>
+                        <div className="py-1" role="none">
+                          {category &&
+                            category.map((genre) => (
+                              <div>
+                                <Link
+                                  key={genre.id}
+                                  to={`/category/${genre.id}`}
+                                >
+                                  {genre.name}
+                                </Link>
+                              </div>
+                            ))}
+                          {/* <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">{category}</a> */}
+                        </div>
+                      </div>
+                    </div>
+
                     <li>
                       <Link
                         to="/"
-                        className="titles hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                        className="titles inline-flex hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                       >
                         Show TV
                       </Link>
