28e9358cd9b5b8e26fdc7739ce7be23c1c5192c0
Dangiras <dan.hausmans@gmail.com>
2022-10-21 23:22:12 +0200

    finished algo for answers

--- src/components/guide/Guide.jsx
+++ src/components/guide/Guide.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Guide.scss";
 
 const Guide = () => {
@@ -6,76 +6,65 @@ const Guide = () => {
     {
       questionText: "What category is giving you the vibe for tonight?",
       answerOptions: [
-        { answerText: "Action", isCorrect: false },
-        { answerText: "Romance", isCorrect: false },
-        { answerText: "Sci-Fi", isCorrect: true },
-        { answerText: "Horror", isCorrect: false },
-        { answerText: "Other", isCorrect: false },
+        { answerText: "Action" },
+        { answerText: "Romance" },
+        { answerText: "Sci-Fi" },
+        { answerText: "Horror" },
+        { answerText: "Other" },
       ],
     },
     {
       questionText: "Which do you prefer?",
       answerOptions: [
-        { answerText: "Latest Movies", isCorrect: false },
-        { answerText: "Older Movies", isCorrect: false },
+        { answerText: "Latest Movies" },
+        { answerText: "Older Movies" },
       ],
     },
     {
       questionText: "Do you want high rated movies.",
       answerOptions: [
-        { answerText: "4 stars +", isCorrect: true },
-        { answerText: "3 stars +", isCorrect: true },
-        { answerText: "2 stars +", isCorrect: true },
-        { answerText: "Whatever", isCorrect: true },
+        { answerText: "4 stars +" },
+        { answerText: "3 stars +" },
+        { answerText: "2 stars +" },
+        { answerText: "Whatever" },
       ],
     },
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [categoryAnswer, setCategoryAnswer] = useState(0);
-  const [timeAnswer, setTimeAnswer] = useState(0);
-  const [ratedAnswer, setRatingAnswer] = useState(0);
+  const [currentAnswer, setCurrentAnswer] = useState("");
+  const [guideAnswers, setGuideAnswers] = useState([]);
 
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
-
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
-    }
+  const toggleQuestion = (e) => {
+    if (!guideAnswers.includes(e.target.value))
+      setGuideAnswers([...guideAnswers, e.target.value]);
 
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
-    } else {
-      setShowScore(true);
     }
   };
+
+  useEffect(() => {
+    console.log(guideAnswers);
+  }, [guideAnswers]);
   return (
     <div className="app">
-      {showScore ? (
-        <div className="score-section"></div>
-      ) : (
-        <>
-          <div className="question-section">
-            <div className="question-count">
-              <span>Question {currentQuestion + 1}</span>/{questions.length}
-            </div>
-            <div className="question-text">
-              {questions[currentQuestion].questionText}
-            </div>
-          </div>
-          <div className="answer-section">
-            {questions[currentQuestion].answerOptions.map((answerOption) => (
-              <button
-                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
-              >
-                {answerOption.answerText}
-              </button>
-            ))}
-          </div>
-        </>
-      )}
+      <div className="question-section">
+        <div className="question-count">
+          <span>Question {currentQuestion + 1}</span>/{questions.length}
+        </div>
+        <div className="question-text">
+          {questions[currentQuestion].questionText}
+        </div>
+      </div>
+      <div className="answer-section">
+        {questions[currentQuestion].answerOptions.map((answerOption) => (
+          <button value={answerOption.answerText} onClick={toggleQuestion}>
+            {answerOption.answerText}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
