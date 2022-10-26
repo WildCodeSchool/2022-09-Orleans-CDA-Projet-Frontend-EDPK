bc541ed0f26cacc6b8b3d18ccc59303a4fcaf744
kylian moreira <kylianmoreira@gmail.com>
2022-10-26 12:28:13 +0200

    add hidden class

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = () => {
                         className={`${
                           isShow
                             ? "transition ease-out duration-100 transform opacity-100 scale-100"
-                            : "transition ease-in duration-75 transform opacity-0 scale-95"
+                            : "transition ease-in duration-75 transform opacity-0 scale-95 hidden"
                         } w-full sm:w-auto md:w-auto absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
                         role="menu"
                         aria-orientation="vertical"
