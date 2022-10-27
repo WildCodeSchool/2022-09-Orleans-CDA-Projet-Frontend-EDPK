b36bb7fd70a6b10b6c7a01422e22fb9aec59df5a
Dangiras <dan.hausmans@gmail.com>
2022-10-27 16:58:40 +0200

    added new logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
                 <img
-                  className="hidden h-8 w-auto lg:block"
+                  className="logo hidden lg:block"
                   src="/images/logo.png"
                   alt="CineFlix"
                 />
