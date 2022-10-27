57d41505574d2b5e6d755ed5b321b42978abc523
kylian moreira <kylianmoreira@gmail.com>
2022-10-27 09:51:11 +0200

    fix mobile version

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
                 type="button"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
-                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="inline-flex items-center justify-center rounded-md w-10 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               >
                 <span className="sr-only">Open main menu</span>
                 <svg
