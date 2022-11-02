e1534790912f1f0fa80ab38aef079f153e89c44c
Paul <108493821+paul-chatelain-fernandes@users.noreply.github.com>
2022-11-02 14:05:41 +0100

    added 404 page structure and style (#52)
    * added 404 page structure and style
    
    * fix bad translation

---
+++ src/pages/error404/Error404.jsx
@@ -0,0 +1,18 @@
+import "./Error404.scss";
+import { Link } from "react-router-dom";
+
+const Error404 = () => {
+  return (
+    <div className="error404">
+      <div className="error404_title">Error 404</div>
+      <div className="error404_text">
+        Sorry the page you are looking for does not exist!
+      </div>
+      <Link to="/" className="error404_back">
+        Go back to home page
+      </Link>
+    </div>
+  );
+};
+
+export default Error404;
