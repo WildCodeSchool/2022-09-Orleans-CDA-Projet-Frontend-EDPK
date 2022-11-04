c542dd48e717d0446256b7417faa624203e81308
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2022-11-04 12:31:03 +0100

    fix others react declarations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Carousel from "./carousel/Carousel";
 import Categories from "./Categories";
 import VideoPlayer from "./VideoPlayer";
