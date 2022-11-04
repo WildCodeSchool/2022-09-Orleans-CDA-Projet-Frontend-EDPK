bf040f5ef1d2ef92bbc3b31672e08459b343f80c
Emmanuel PEREIRA <epereira777@gmail.com>
2022-11-04 09:56:48 +0100

    fix space

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -42,7 +42,7 @@ const Popup = () => {
                     className="text-lg font-medium leading-6 text-gray-900"
                     id="modal-title"
                   >
-                    Warning adult content !
+                    Warning adult content!
                   </h3>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
